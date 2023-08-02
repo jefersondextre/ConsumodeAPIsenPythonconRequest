4d00fe7c4f4f0674765114165565be930141dee6
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2023-08-02 15:40:52 -0500

    Enviando headers en formato Json con metodo GET

--- main.py
+++ main.py
@@ -8,31 +8,9 @@ import json
 
 def get_data():
     try:
-        # response = requests.get('https://www.diresacallao.gob.pe/wdiresa/portal/')
-        # print(response.content)
-        # response = requests.get('https://pokeapi.co/api/v2/pokemon/')
-        # print(response.status_code, response.content)
-        # data={
-        #     "title":"new product",
-        #     "description" : "new product",
-        #     "price": 12.50,
-        #     "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-        #     "category": "electronic"
-        # }
-        # data={
-        #     "username":"johnd",
-        #     "password":"m38rmF$"
-        # }
-        # data = {
-        #     "title":"test product",
-        #     "price":25.50,
-        #     "descripcion": "test"
-        # }
-        id=input("Ingrese el id del usuario a eliminar: ")
-        response=requests.delete(f"https://fakestoreapi.com/users/{id}")
-        # response = requests.post('https://fakestoreapi.com/auth/login',data)
+        headers = { "Content-Type":"application/json:charset = utf-8"}
+        response=requests.get(f'https://fakestoreapi.com/carts/6',headers=headers)
         print(json.dumps(response.json(),indent=4))
-        print('usuario eliminado.')
     except requests.exceptions.RequestException as err:
         print(f"Ha ocurrido un error: {err}")
         

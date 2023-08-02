b729f151e1a189afcbebe05f2951bcc301ec8be7
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2023-08-02 10:43:03 -0500

    usando el manejador de errores de paquete request de  encontrarse un error e imprimiendolo por consola

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import requests
 import json
 
 def get_data():
-    # response = requests.get('https://www.diresacallao.gob.pe/wdiresa/portal/')
-    # print(response.content)
-    # response = requests.get('https://pokeapi.co/api/v2/pokemon/')
-    # print(response.status_code, response.content)
-    response = requests.get('https://pokeapi.co/api/v2/pokemon/')
-    print(json.dumps(response.json(),indent=4))
-
+    try:
+        # response = requests.get('https://www.diresacallao.gob.pe/wdiresa/portal/')
+        # print(response.content)
+        # response = requests.get('https://pokeapi.co/api/v2/pokemon/')
+        # print(response.status_code, response.content)
+        response = requests.get('https://pokeapi.co/api/v2/pokemon/')
+        print(json.dumps(response.json(),indent=4))
+    except requests.exceptions.RequestException as err:
+        print(f"Ha ocurrido un error: {err}")
+        
 if __name__ == "__main__":
     get_data()
     
